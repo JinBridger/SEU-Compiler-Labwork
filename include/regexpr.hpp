2219bf4f752fec02a353a4a09b896c3c2ce42668
jinbridge <2635480475@qq.com>
2023-12-11 22:09:48 +0800

    feat: regexpr

---
+++ include/regexpr.hpp
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <stack>
+#include <string>
+
+// convert to postfix expr
+class regexpr {
+public:
+    static std::string convert(std::string regexpr) {
+        return convert_to_postfix(add_connect_operator(regexpr));
+    }
+
+private:
+    // operator priority: * > | > @
+    static std::string add_connect_operator(std::string regexpr) {
+        std::string new_regexpr = "";
+        new_regexpr += regexpr[0];
+        for (int i = 1; i < regexpr.length(); ++i) {
+            bool add_connect = false;
+
+            char curr_char = regexpr[i];
+            char last_char = new_regexpr[new_regexpr.length() - 1];
+
+            // case 1: add between 2 non-operator character
+            if (!is_regexpr_operator(last_char) && !is_regexpr_operator(curr_char))
+                add_connect = true;
+
+            // case 2: add between non-operator and '('
+            if (!is_regexpr_operator(last_char) && curr_char == '(')
+                add_connect = true;
+
+            // case 3: add between ')' and non-operator
+            if (last_char == ')' && !is_regexpr_operator(curr_char))
+                add_connect = true;
+
+            // case 4: add between '*' and non-operator
+            if (last_char == '*' && !is_regexpr_operator(curr_char))
+                add_connect = true;
+
+            // case 5: add between '*' and '('
+            if (last_char == '*' && curr_char == '(')
+                add_connect = true;
+
+            if (add_connect)
+                new_regexpr += '@';
+
+            new_regexpr += curr_char;
+        }
+        return new_regexpr;
+    }
+
+    static std::string convert_to_postfix(std::string regexpr) {
+        std::stack<char> operator_stack;
+        std::stack<char> intermediate_stack;
+        for (int i = 0; i < regexpr.length(); ++i) {
+            // non-operator
+            if (!is_regexpr_operator(regexpr[i])) {
+                intermediate_stack.push(regexpr[i]);
+                continue;
+            }
+
+            // left parenthese
+            if (regexpr[i] == '(') {
+                operator_stack.push(regexpr[i]);
+                continue;
+            }
+
+            // right parenthese
+            if (regexpr[i] == ')') {
+                while (operator_stack.top() != '(') {
+                    intermediate_stack.push(operator_stack.top());
+                    operator_stack.pop();
+                }
+                operator_stack.pop();
+                continue;
+            }
+
+            // other operators
+            while (!operator_stack.empty() && operator_stack.top() != '('
+                   && !compare_priority(regexpr[i], operator_stack.top())) {
+                intermediate_stack.push(operator_stack.top());
+                operator_stack.pop();
+            }
+
+            operator_stack.push(regexpr[i]);
+        }
+        while (!operator_stack.empty()) {
+            intermediate_stack.push(operator_stack.top());
+            operator_stack.pop();
+        }
+
+        std::stack<char> revert_stack;
+        while (!intermediate_stack.empty()) {
+            revert_stack.push(intermediate_stack.top());
+            intermediate_stack.pop();
+        }
+
+        std::string new_regexpr = "";
+        while (!revert_stack.empty()) {
+            new_regexpr += revert_stack.top();
+            revert_stack.pop();
+        }
+
+        return new_regexpr;
+    }
+
+    static bool is_regexpr_operator(char c) {
+        if (c == '*' || c == '|' || c == '(' || c == ')' || c == '@')
+            return true;
+        return false;
+    }
+
+    // return true when a has higher priority than b
+    static bool compare_priority(char a, char b) {
+        auto convert_int = [](char a) -> int {
+            switch (a) {
+            case '(':
+                return 0;
+            case '*':
+                return -1;
+            case '|':
+                return -2;
+            case '@':
+                return -3;
+            }
+            return 1;
+        };
+
+        return convert_int(a) > convert_int(b);
+    }
+};
--- src/lexical_analyzer.cpp
+++ src/lexical_analyzer.cpp
@@ -1,16 +1,15 @@
 #include "dfa.hpp"
 #include "lexical_rules.hpp"
+#include "regexpr.hpp"
 
 #include <iostream>
 #include <string>
 
+void test() {
+    std::string a = "aa*(bab*a)*(a|b)b*";
+    std::cout << "postf: " << regexpr::convert(a) << std::endl;
+}
+
 int main() {
-    std::string id;
-    std::cin >> id;
-    std::cout << "id: " + id << std::endl;
-
-    auto test = dfa(identifier_rules, identifier_accept_status);
-
-    std::string result = test.verify(id) ? "true" : "false";
-    std::cout << "result: " + result;
+    test();
 }
